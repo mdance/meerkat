62bd204781571bebc5be03176b538a7e8c3f813a
Michael Dance <michael.dance@gmail.com>
2010-03-07 00:24:04 +1300

    Initial import

---
+++ meerkat.tpl.php
@@ -0,0 +1,15 @@
+<?php
+// $Id$
+
+/**
+ * @file meerkat.tpl.php
+ * Default theme implementation for meerkat content.
+ *
+ * Available variables:
+ * - $input: The meerkat container content.
+ * - $options: The meerkat options.
+ */
+?>
+<div id="meerkat">
+  <?php print $input; ?>
+</div>
