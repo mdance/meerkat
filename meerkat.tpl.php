872d5c604918cbce0c10e06827408dc2d868e0ab
Michael Dance <michael.dance@gmail.com>
2010-03-07 21:54:21 +1300

    Latest revision

--- meerkat.tpl.php
+++ meerkat.tpl.php
@@ -9,7 +9,38 @@
  * - $input: The meerkat container content.
  * - $options: The meerkat options.
  */
+
+$css = '';
+$close = '';
+$dontshow = '';
+
+if ( $options['css'] != '' ) {
+  $css = ' class="' . $options['css'] . '"';
+}
+
+if ( $options['close'] != 'none' && $options['showclose'] == 'true' ) {
+  $title = t('Click here to close this');
+
+  $close = <<<EOF
+  <a href="#" class="meerkatclose" title="$title">Close</a>
+EOF;
+}
+
+if ( $options['dontShow'] != 'none' && $options['showdontshow'] == 'true'  ) {
+  $title = t('Click here to close this and future messages');
+
+  $dontshow = <<<EOF
+  <a href="#" class="meerkatdontshow" title="$title">Don't Show</a>
+EOF;
+}
+
 ?>
-<div id="meerkat">
-  <?php print $input; ?>
+<div id="meerkat"<?php echo $css; ?>>
+  <?php
+
+  echo $close;
+  echo $dontshow;
+  echo $input;
+
+  ?>
 </div>
